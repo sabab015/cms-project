f56afdc8761ff1a23390fe33b5741f17b1eec06b
AL SABAB <sabab015@gmail.com>
2022-10-24 17:04:57 +0600

    img fetched from database

--- index.php
+++ index.php
@@ -36,12 +36,12 @@
                 </p>
                 <p><span class="glyphicon glyphicon-time"></span><?php echo $post_date ?></p>
                 <hr>
-                <img class="img-responsive" src="images/picture1.jpg" alt="">
+                <img class="img-responsive" src="images/<?php echo $post_image ?>" alt="">
                 <hr>
                 <p><?php echo $post_content ?></p>
                 <a class="btn btn-primary" href="#">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
-
                 <hr>
+
             <?php } ?>
 
 
