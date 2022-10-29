f04070d0990c3c58e7fddf94bf8f6be71fc23116
AL SABAB <sabab015@gmail.com>
2022-10-29 13:46:55 +0600

    searchbar added

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -1,16 +1,12 @@
 <div class="col-md-4">
 
-    <?php 
-        if(isset($_POST['submit'])){
-            echo $search = $_POST['search'];
-        }
-    ?>
+
 
 
     <!-- Blog Search Well -->
     <div class="well">
         <h4>Blog Search</h4>
-        <form action="" method="POST">
+        <form action="search.php" method="POST">
             <div class="input-group">
                 <input name="search" type="text" class="form-control">
                 <span class="input-group-btn">
@@ -25,18 +21,24 @@
 
     <!-- Blog Categories Well -->
     <div class="well">
+
+        <?php
+        $query = "SELECT * FROM categories";
+        $select_all_categories_sidebar = mysqli_query($connection, $query);
+        ?>
+
+
+
+
         <h4>Blog Categories</h4>
         <div class="row">
             <div class="col-lg-6">
                 <ul class="list-unstyled">
-                    <li><a href="#">Category Name</a>
-                    </li>
-                    <li><a href="#">Category Name</a>
-                    </li>
-                    <li><a href="#">Category Name</a>
-                    </li>
-                    <li><a href="#">Category Name</a>
-                    </li>
+                    <?php
+                    while ($row = mysqli_fetch_assoc($select_all_categories_sidebar)) {
+                        $cat_title = $row['cat_title'];
+                        echo "<li><a href='#'>{$cat_title}</a></li>";
+                    } ?>
                 </ul>
             </div>
             <!-- /.col-lg-6 -->
@@ -58,9 +60,6 @@
     </div>
 
     <!-- Side Widget Well -->
-    <div class="well">
-        <h4>Side Widget Well</h4>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore, perspiciatis adipisci accusamus laudantium odit aliquam repellat tempore quos aspernatur vero.</p>
-    </div>
+    <?php include "widget.php" ?>
 
 </div>
